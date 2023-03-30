62efb435ab958048a6606c93cff669ff3c82e9ca
SHLee17 <azurite17@gmail.com>
2023-03-31 05:37:25 +0900

    First

---
+++ Agent Two-way Communication (Type1)/Agent Two-way Communication (Type1)/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        private const int PORT = 7777;
+        private static ConcurrentDictionary<int, TcpClient> clients = new ConcurrentDictionary<int, TcpClient>();
+        private static int clientIdCounter = 0;
+
+        static async Task Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, PORT);
+            listener.Start();
+            Console.WriteLine($"Waiting for clients on port {PORT}...");
+
+            string previousFilePath = null;
+
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                clientIdCounter++;
+                clients.TryAdd(clientIdCounter, client);
+                Console.WriteLine($"Client {clientIdCounter} connected");
+                // 클라이언트 핸들러 생성 및 시작
+                _ = Task.Run(() => ClientHandler(clientIdCounter, client));
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync("http://r741.realserver2.com/api/post.php");
+                    response.EnsureSuccessStatusCode();
+                    string currentValue = await response.Content.ReadAsStringAsync();
+                    if (previousFilePath != currentValue && currentValue != null)
+                    {
+                        SendMessageToAllClients(currentValue);
+                        previousFilePath = currentValue;
+                    }
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+                }
+            }
+        }
+
+        static void SendMessageToAllClients(string message)
+        {
+            byte[] sendBytes = Encoding.ASCII.GetBytes(message);
+            foreach (TcpClient client in clients.Values)
+            {
+                client.GetStream().Write(sendBytes, 0, sendBytes.Length);
+            }
+        }
+
+        private static async void ClientHandler(int clientId, TcpClient client)
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] bytes = new byte[1024];
+                    int bytesRead = await client.GetStream().ReadAsync(bytes, 0, bytes.Length);
+                    string message = Encoding.ASCII.GetString(bytes, 0, bytesRead);
+                    Console.WriteLine($"Received message from client {clientId}: {message}");
+
+                    using (HttpClient httpClient = new HttpClient())
+                    {
+                        string url = "http://r741.realserver2.com/api/testJSON.php";
+                        HttpContent content = new StringContent(message, Encoding.UTF8, "text/plain");
+                        HttpResponseMessage response = await httpClient.PostAsync(url, content);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var responseBody = await response.Content.ReadAsStringAsync();
+                            Console.WriteLine(responseBody);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error: {response.StatusCode}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                clients.TryRemove(clientId, out _);
+                Console.WriteLine($"Client {clientId} disconnected");
+            }
+        }
+    }
+}
+
+//Task.Run(async () =>
+//{
+//    while (true)
+//    {
+//        ConsoleKeyInfo keyInfo = Console.ReadKey();
+//        if (keyInfo.Key == ConsoleKey.Enter)
+//        {
+//            SendMessageToAllClients("Hello, clients!");
+//        }
+//    }
+//});
---
+++ TCA Client/TCA Client/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // 서버에 접속
+            TcpClient client = new TcpClient();
+            await client.ConnectAsync(IPAddress.Loopback, 7777);
+
+            Console.WriteLine("서버에 접속되었습니다.");
+
+            // 서버에서 보낸 메시지를 받아들이는 Task를 실행
+            _ = ReceiveMessages(client);
+
+            // 메시지 입력을 대기
+            while (true)
+            {
+                Console.Write("\n메시지 입력: ");
+                string message = Console.ReadLine();
+                if (message.ToLower() == "exit")
+                    break;
+
+                // 서버로 메시지 전송
+                SendMessageToServer(client, message);
+            }
+
+            // 서버와의 연결 종료
+            client.Close();
+            Console.WriteLine("서버와의 연결이 종료되었습니다.");
+        }
+
+        static void SendMessageToServer(TcpClient client, string message)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] bytesToSend = Encoding.UTF8.GetBytes(message);
+            stream.Write(bytesToSend, 0, bytesToSend.Length);
+        }
+
+        static async Task ReceiveMessages(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                // 서버로부터 메시지를 받음
+                int byteCount = await stream.ReadAsync(buffer, 0, buffer.Length);
+                string receivedMessage = Encoding.UTF8.GetString(buffer, 0, byteCount);
+
+                // 메시지 출력
+                Console.WriteLine($"\n서버: {receivedMessage}");
+            }
+        }
+    }
+}
